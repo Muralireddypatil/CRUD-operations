ab9b98e728551fc2e13b9a726ffe6ae5d308dadd
Muralireddypatil <109702050+Muralireddypatil@users.noreply.github.com>
2025-12-01 00:08:24 +0000

    helper function for multiple rows

--- server/database.js
+++ server/database.js
@@ -46,3 +46,15 @@ const getRow = (query, params = []) => {
     });
   });
 };
+// Helper function to get multiple rows
+const getAllRows = (query, params = []) => {
+  return new Promise((resolve, reject) => {
+    db.all(query, params, (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
+  });
+};
